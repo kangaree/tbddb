cf253e32aeb3a3b5ab00cb1f9ee00498def84ccd
Gary Kang <garyzkang@gmail.com>
2019-04-16 15:06:35 -0400

    Setup Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const birthdays = require("./routes/api/birthdays");
+const passport = require('passport');
+require('./config/passport')(passport);
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -14,9 +16,10 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello world"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use("/api/users", users);
 app.use("/api/birthdays", birthdays);
+
+app.use(passport.initialize());
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,42 +4,51 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
-router.post('/register', (req, res) => {
+// users.js
+router.post("/register", (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
 
     if (!isValid) {
         return res.status(400).json(errors);
     }
 
-    // Check to make sure nobody has already registered with a duplicate email
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (user) {
-                // Throw a 400 error if the email address already exists
-                return res.status(400).json({ email: "A user has already registered with this address" })
-            } else {
-                // Otherwise create a new user
-                const newUser = new User({
-                    handle: req.body.handle,
-                    email: req.body.email,
-                    password: req.body.password
-                })
+    User.findOne({ name: req.body.name }).then(user => {
+        if (user) {
+            errors.name = "User already exists";
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
 
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
-                        newUser.password = hash;
-                        newUser.save()
-                            .then(user => res.json(user))
-                            .catch(err => console.log(err));
-                    })
-                })
-            }
-        })
-})
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => {
+                            const payload = { id: user.id, name: user.name };
+
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
 
 router.post('/login', (req, res) => {
     const { errors, isValid } = validateLoginInput(req.body);
@@ -78,6 +87,14 @@ router.post('/login', (req, res) => {
                     }
                 });
         });
+});
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
 });
 
 module.exports = router;
