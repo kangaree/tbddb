3220a967f773af510a496163cf0029870f521333
Gary Kang <garyzkang@gmail.com>
2019-04-17 18:31:58 -0400

    Add Sort to Birthday Backend Route

--- routes/api/birthdays.js
+++ routes/api/birthdays.js
@@ -6,6 +6,7 @@ router.get("/test", (req, res) => res.json({ msg: "This is the birthdays route" 
 
 router.get('/:month/:day', (req, res) => {
     Birthday.find({ month: parseInt(req.params.month), day: parseInt(req.params.day) })
+        .sort({ wikilinks: -1 })
         .then(birthday => res.json(birthday))
         .catch(err =>
             res.status(404).json({ nobirthdayfound: 'No birthday found with that date' })
